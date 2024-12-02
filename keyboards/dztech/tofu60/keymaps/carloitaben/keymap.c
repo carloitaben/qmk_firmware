5eebadc3aafbdefa5c37879603061df549775faf
Carlo <hola.carlodominguez@gmail.com>
2024-12-02 20:18:55 +0100

    feat: combos

--- keyboards/dztech/tofu60/keymaps/carloitaben/keymap.c
+++ keyboards/dztech/tofu60/keymaps/carloitaben/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC,
         LGUI_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENTER,
         KC_LSFT,          _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,
-        XXXXXXX, XXXXXXX, KC_LCTL, KC_LALT,                            LT(_FN, KC_SPC),                                      XXXXXXX, KC_RALT, LCAG(KC_RGUI),
+        XXXXXXX, XXXXXXX, KC_LCTL, KC_LALT,                            LT(_FN, KC_SPC),                                      XXXXXXX, KC_RALT, KC_RCTL,
     ),
     [_FN] = LAYOUT_all(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_VOLD, KC_VOLU,
@@ -44,6 +44,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, KC_LGUI, _______,                            KC_SPC,                                      _______, KC_RALT, KC_RGUI,
     ),
+};
+
+const uint16_t PROGMEM combo_gaming_enter[] = {KC_LCTL, KC_RCTL, COMBO_END};
+const uint16_t PROGMEM combo_gaming_leave[] = {KC_LGUI, KC_RGUI, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(combo_gaming_enter, TG(_GAMING)),
+    COMBO(combo_gaming_leave, TG(_GAMING)),
 };
 
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
